27f4c4cd1af5e8fe13d2228c600ee50bf42658cf
kulny <wordworthathousandpictures@gmail.com>
2018-09-19 13:43:40 -0400

    Add project files.

---
+++ csis112Lab3/Scores.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Scores.h"
+
+
+Scores::Scores()
+{
+}
+
+
+Scores::~Scores()
+{
+}
---
+++ csis112Lab3/Scores.h
@@ -0,0 +1,16 @@
+#pragma once
+class Scores
+{
+public:
+	Scores();
+	// init scores at val
+	// init scores at 0
+	// add value to array
+
+
+private:
+	// sort array in ascending order
+	// computer average of scores
+	// display scores 
+};
+

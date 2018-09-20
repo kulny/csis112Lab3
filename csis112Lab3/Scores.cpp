bb233a3da8b492eaf3dada170d2b02e113abff80
kulny <wordworthathousandpictures@gmail.com>
2018-09-20 17:32:11 -0400

    score class and some fstream

--- csis112Lab3/Scores.cpp
+++ csis112Lab3/Scores.cpp
@@ -1,12 +1,46 @@
 #include "stdafx.h"
 #include "Scores.h"
+#include <iostream>
+
 
 
 Scores::Scores()
 {
+	for (int i = 0; i < 100; i++)
+	{
+		scoreArray[i] = 0;
+	}
 }
 
-
-Scores::~Scores()
+Scores::Scores(double val)
 {
+	for (int i = 0; i < 100; i++)
+	{
+		scoreArray[i] = val;
+	}
+}
+
+void Scores::AddValue(double val)
+{
+	scoreArray[index] = val;
+	index++; // keeps track of location where next added value should go
+}
+
+double Scores::ComputeAvg()
+{
+	double avg;
+	for (int i = 0; i < 100; i++)
+	{
+		avg += scoreArray[i];
+	}
+	return avg / (index == 0 ? 100 : index); // divides by 100 if no values have been added (for example, all grades init to some val)
+}
+
+void Scores::DisplayScores()
+{
+	for (int i = 0; i <= index; i++)
+	{
+		std::cout << scoreArray[i];
+	}
+	std::cout << ComputeAvg();
 }
--- csis112Lab3/Scores.h
+++ csis112Lab3/Scores.h
@@ -2,13 +2,20 @@
 class Scores
 {
 public:
-	Scores();
-	// init scores at val
-	// init scores at 0
-	// add value to array
-
+	Scores(); // init scores at 0
+	Scores(double val);	// init scores at val
+	void AddValue(double val); // add value to array
 
 private:
+	// vars
+	double scoreArray[100];
+	int index = 0; // used to add values to array by iteration 
+	int index = 0; // used to keep track of how many grades added to array for avg purposes
+
+	// Operations and other
+	void SortArray();
+	double ComputeAvg();
+	void DisplayScores();
 	// sort array in ascending order
 	// computer average of scores
 	// display scores 
